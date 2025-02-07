37e6dd375484835445166224d53f077acbbd25e2
noracostea <noracostea33@gmail.com>
2025-02-07 11:28:51 +0100

    Added file read examples

---
+++ file_example_read.py
@@ -0,0 +1,17 @@
+fp = open("text.txt", "r")
+print(fp.read())
+fp.close() # close is good practice, not needed in this example
+
+# same thing, but more pythonic
+with open("text.txt", "r") as fp: # this creates a context
+    print(fp.read())
+
+print("we are done, the context closed by the indent")
+
+# read from the same file, line by line
+with open("text.txt", "r") as fp:
+    line_number = 1
+    for line in fp:
+        # print(line, end="")
+        print(f"{line_number}: {line.rstrip()}")
+        line_number += 1
